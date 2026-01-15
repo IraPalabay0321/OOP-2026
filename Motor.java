9ca9e18222dadad49821823f546ee81cb91d570e
IraPalabay0321 <iramishael.palabay@lorma.edu>
2026-01-15 12:18:07 +0800

    motorcycle

---
+++ Motor.java
@@ -0,0 +1,15 @@
+public class Motor {
+    String brand;
+    String version;
+    int model;
+    int cc;
+    char capacity;
+    String condition;
+
+    void printDetails(){
+        System.out.printf("""
+            %s %s %d
+                
+                """,brand,version,cc );
+    }
+}
---
+++ main.java
@@ -0,0 +1,25 @@
+public class main{
+    public static void main(String[]args){
+        Motor ver1 = new Motor();
+        
+        ver1.brand = "Yamaha";
+        ver1.version = "Nmax";
+        ver1.model = 2024;
+        ver1.cc =  155;
+        ver1.capacity = '2';
+        ver1.condition = "secondHand";
+
+
+        var ver2 = new Motor();
+        
+        ver2.brand = "Honda";
+        ver2.version = "Adv";
+        ver2.model = 2025;
+        ver2.cc =  160;
+        ver2.capacity = '2';
+        ver2.condition = "new";
+
+        ver1.printDetails();
+        ver2.printDetails();
+    }
+}
